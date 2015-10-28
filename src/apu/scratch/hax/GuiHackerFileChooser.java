a853cbb8d3a33f75b7f309c4206a070dba19e524
MegaApuTurkUltra <apu@aputurk.tk>
2015-10-27 20:09:23 -0400

    Fixes

--- src/apu/scratch/hax/GuiHackerFileChooser.java
+++ src/apu/scratch/hax/GuiHackerFileChooser.java
@@ -8,18 +8,30 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
+import javax.imageio.ImageWriter;
 import javax.imageio.metadata.IIOMetadataNode;
 import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.FileImageOutputStream;
 import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
@@ -128,8 +140,18 @@ public class GuiHackerFileChooser extends JPanel {
 						File f = fileChooser.getSelectedFile();
 						if (f == null || f.isDirectory() || !f.exists())
 							return;
-						if (type == FileFilterType.GIF && !checkGifFrameRate(f))
-							return;
+						if (type == FileFilterType.GIF) {
+							if (!checkGifFrameRate(f))
+								return;
+							
+//							try {
+//								f = watermark(f);
+//							} catch (Exception e) {
+//								e.printStackTrace();
+//								ThumbnailGuiHacker.INSTANCE.showExceptionDialog(e);
+//							}
+						}
+
 						ThumbnailGuiHacker.INSTANCE.executeHack(f, type.mime);
 					}
 				});
@@ -175,6 +197,101 @@ public class GuiHackerFileChooser extends JPanel {
 			ThumbnailGuiHacker.INSTANCE.showExceptionDialog(e);
 		}
 		return true;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		File test = new File("sample path");
+		File wm = watermark(test);
+		Files.copy(Paths.get(wm.toURI()), new FileOutputStream("test.gif"));
+	}
+
+	/*
+	 * java, y u make dis so complicated?
+	 */
+	public static File watermark(File gif) throws Exception {
+		ImageInputStream iin = new FileImageInputStream(gif);
+		File tmp = File.createTempFile("thumb_", ".gif");
+		tmp.deleteOnExit();
+		ImageOutputStream iout = new FileImageOutputStream(tmp);
+
+		InputStream wmarkin = ThumbnailGuiHacker.class
+				.getResourceAsStream("/apu/scratch/hax/watermark.png");
+		BufferedImage wmark = ImageIO.read(wmarkin);
+		wmarkin.close();
+
+		Iterator<ImageReader> readers = ImageIO.getImageReaders(iin);
+
+		ImageReader reader = null;
+		while (readers.hasNext()) {
+			reader = readers.next();
+
+			String metaFormat = reader.getOriginatingProvider()
+					.getNativeImageMetadataFormatName();
+			if ("gif".equalsIgnoreCase(reader.getFormatName())
+					&& !"javax_imageio_gif_image_1.0".equals(metaFormat)) {
+				continue;
+			} else {
+				break;
+			}
+		}
+		if (reader == null) {
+			iout.close();
+			throw new IOException("Can not read image format!");
+		}
+
+		ImageWriter writer = ImageIO.getImageWriter(reader);
+		writer.setOutput(iout);
+
+		boolean isGif = reader.getFormatName().equalsIgnoreCase("gif");
+		reader.setInput(iin, false, !isGif);
+		writer.prepareWriteSequence(reader.getStreamMetadata());
+		for (int index = 0;; index++) {
+			try {
+				// read a frame and its metadata
+				IIOImage frame = reader.readAll(index, null);
+				RenderedImage img = frame.getRenderedImage();
+				if (img == null)
+					throw new IllegalStateException("img == null");
+				BufferedImage newImg = convertRenderedImage(img);
+				newImg.getGraphics().drawImage(wmark, 0, 0, newImg.getWidth(),
+						newImg.getHeight(), 0, 0, wmark.getWidth(),
+						wmark.getHeight(), null);
+				frame.setRenderedImage(newImg);
+				writer.writeToSequence(frame, null);
+			} catch (IndexOutOfBoundsException e) {
+				break;
+			}
+		}
+
+		// clean up
+		reader.dispose();
+		iout.close();
+		writer.dispose();
+		Files.copy(Paths.get(tmp.toURI()), new FileOutputStream("test.gif"));
+		return tmp;
+	}
+
+	public static BufferedImage convertRenderedImage(RenderedImage img) {
+		if (img instanceof BufferedImage) {
+			return (BufferedImage) img;
+		}
+		ColorModel cm = img.getColorModel();
+		int width = img.getWidth();
+		int height = img.getHeight();
+		WritableRaster raster = cm
+				.createCompatibleWritableRaster(width, height);
+		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+		Hashtable<String, Object> properties = new Hashtable<>();
+		String[] keys = img.getPropertyNames();
+		if (keys != null) {
+			for (int i = 0; i < keys.length; i++) {
+				properties.put(keys[i], img.getProperty(keys[i]));
+			}
+		}
+		BufferedImage result = new BufferedImage(cm, raster,
+				isAlphaPremultiplied, properties);
+		img.copyData(raster);
+		return result;
 	}
 
 	private List<Integer> getGifDelays(ImageInputStream imageStream)
